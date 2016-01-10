49252c0d6444c2994f36c167a6dc4d33e74cc6ef
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-01-10 15:45:30 +0000

    Added InputMappings class

---
+++ src/TrekWars/InputMappings.java
@@ -0,0 +1,17 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrekWars;
+
+/**
+ *
+ * @author Andy.Kuszyk
+ */
+public class InputMappings {
+    public final String left = "Left";
+    public final String right = "Right";
+    public final String accelerate = "Accelerate";
+    public final String decelerate = "Decelerate";
+    public final String fire = "Fire";
+}
