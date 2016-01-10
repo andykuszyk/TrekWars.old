8a4714da2a82ec84d6f1d3ff242ee38cf89777e5
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-01-10 15:46:55 +0000

    Made fields static

--- src/TrekWars/InputMappings.java
+++ src/TrekWars/InputMappings.java
@@ -9,9 +9,9 @@ package TrekWars;
  * @author Andy.Kuszyk
  */
 public class InputMappings {
-    public final String left = "Left";
-    public final String right = "Right";
-    public final String accelerate = "Accelerate";
-    public final String decelerate = "Decelerate";
-    public final String fire = "Fire";
+    public static final String left = "Left";
+    public static final String right = "Right";
+    public static final String accelerate = "Accelerate";
+    public static final String decelerate = "Decelerate";
+    public static final String fire = "Fire";
 }
