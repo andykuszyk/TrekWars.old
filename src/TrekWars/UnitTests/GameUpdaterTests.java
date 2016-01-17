2d383f393435f945816c69f31cd3332da60d1037
andy.kuszyk <andy.kuszyk@galleria-rts.com>
2016-01-17 11:52:15 +0000

    Stubbed out initial GameUpdater test

---
+++ src/TrekWars/UnitTests/GameUpdaterTests.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package TrekWars.UnitTests;
+
+import TrekWars.GameUpdater;
+import org.junit.Test;
+
+/**
+ *
+ * @author Andy.Kuszyk
+ */
+public class GameUpdaterTests {
+    
+    @Test
+    public void processUpdate_correctlyPositionsCamera() {
+        
+        GameUpdater gameUpdater = new GameUpdater; 
+        
+    }
+}
